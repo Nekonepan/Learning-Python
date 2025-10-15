e984ad5a5333e996c9c9d746618b6ffee9efdc7f
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-10-15 23:43:40 +0700

    Add Human class with initialization and instance methods for name, age, and gender

---
+++ OOP/OOP2.py
@@ -0,0 +1,16 @@
+class Human:
+  species = "Homo Sapiens"
+
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.gender = gender
+
+x = Human("Edcorner", 30, "Male")
+y = Human("Learning", 32, "Female")
+
+print(x.species)
+print(y.species)
+
+print(f"Hi! My name is {x.name}. I am a {x.gender}, and I am {x.age} years old")
+print(f"Hi! My name is {y.name}. I am a {y.gender}, and I am {y.age} years old")
